8008170aa68f67c258d3c3adec38d4b358a0f34b
luyandan <2103029766@qq.com>
2021-02-02 21:21:12 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    // filename: 'main.js'
+    filename: '[name].[contenthash].js',
+  }
+};
